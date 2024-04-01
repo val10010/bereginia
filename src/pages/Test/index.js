de1d2fb61b238246e464b53f1cd59f55ecc1ea51
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-04-01 13:53:03 +0300

    added

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -1,7 +1,7 @@
 import React, { lazy } from 'react';
 import Header from 'Main/pages/Ivan/components/Header'
 const Footer = lazy(() => import('Components/Footer'));
-import {Test} from 'Main/devlink/Test';
+import { Plans } from 'Main/devlink/Plans';
 
 import style from './style.scss';
 
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <div className={style.main}>
             <Header/>
-            <Test/>
+            <Plans/>
             <Footer email="naturalmedical96@gmail.com" phone="+380 67 484 93 08"/>
         </div>
     );
